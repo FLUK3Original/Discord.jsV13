5fa9277225ea76058c5ed14ad6bc04d069424974
FLUK3 <91425554+FLUK3Original@users.noreply.github.com>
2021-10-27 12:27:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const cfg = require("./cfg.json");
 const { Client, Intents, Collection } = require("discord.js");
-const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]});
+const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], 
+intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, 
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]});
+const prefix = cfg.prefix;
 
 client.commands = new Collection();
 client.aliases = new Collection();
-client.events = new Collection();
+client.event = new Collection();
 client.cooldowns = new Collection();
 
 ["command", "event"].forEach(handler => {
-        require(`./handlers/${handler}`)(client)
+        require(`./handlers/${handler}`)(bot)
 });
+
 client.login(cfg.token);
