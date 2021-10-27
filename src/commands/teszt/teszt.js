297046b2942f52cd9a672740da2e88ccfbc066f6
FLUK3 <91425554+FLUK3Original@users.noreply.github.com>
2021-10-27 12:32:00 +0200

    Update teszt.js

--- src/commands/teszt/teszt.js
+++ src/commands/teszt/teszt.js
@@ -1,16 +1,14 @@
-const discord = require("discord.js")
+const Discord = require(`discord.js`);
 
 module.exports = {
- name: "teszt",
- categories: "teszt",
- description: "video",
- cooldown: "5",
- usage: "hasznals",
- run: async (client, message, args) => {
-     let ping = new discord.MessageEmbed()
-     .setTitle("Szia")
-     .setDescription("PONG!! (XD)")
-     message.channel.send({embeds: [ping]})
- }
-
+    name: "teszt",
+    aliases: ["test"],
+    categories: "Teszt",
+    permissions: "",
+    description: "Teszt parancs",
+    cooldown: 5,
+    usage: "",
+    run: async(bot, message, args) => {
+        message.channel.send({content: "Üdv! Ez egy teszt parancs!"})
+    }
 }
